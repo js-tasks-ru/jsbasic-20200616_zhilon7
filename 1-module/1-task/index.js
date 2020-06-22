811751682c596e76d1b4b90b338918ac0b1745d4
Anton Zhilonov <antonzhilonov@yandex.ru>
2020-06-22 19:13:02 +0300

    факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,17 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  let x = n;
+  if (n == 0) {
+    return 1
+  }
+  for (let i = 1; i < n; i++) {
+    x -= 1
+    result *= x;
+  }
+
+
+  return result;
 }
+factorial(n)
