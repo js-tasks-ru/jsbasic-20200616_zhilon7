d57e8e033be0204f26f2f5eb00cff90e6011fa53
Anton Zhilonov <antonzhilonov@yandex.ru>
2020-06-24 16:48:31 +0300

    Создайте калькулятор2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,13 +1,15 @@
-let calculator = {};
-calculator.read = function (a, b) {
-  calculator.x = a;
-  calculator.y = b;
-};
-calculator.sum = function () {
-  return calculator.x + calculator.y
-};
-calculator.mul = function () {
-  return calculator.x * calculator.y
+let calculator = {
+  read: function (a, b) {
+    calculator.x = a;
+    calculator.y = b;
+  },
+  sum: function () {
+    return calculator.x + calculator.y
+  },
+  mul: function () {
+    return calculator.x * calculator.y
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
