1d71ff821f6d3a030c6042a42a02def2bd6c47ac
Anton Zhilonov <antonzhilonov@yandex.ru>
2020-06-30 15:58:41 +0300

    Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  str.split(',').map(item => item.split(' ')).filter((item, index) => typeof item[index] == number)
+  let result = []
+  str = str.split(',')
+    .map(item => item.split(' ')
+      .filter(item => isFinite(item) && item != '')
+    )
+  result = result.concat(...str).sort((a, b) => b - a)
+  return {
+    min: +result[result.length - 1],
+    max: +result[0]
+  }
 }
