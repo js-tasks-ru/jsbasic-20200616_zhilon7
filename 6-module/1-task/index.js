af018cc3008912d7ec0c54062d0565e2ed2f4435
Anton Zhilonov <antonzhilonov@yandex.ru>
2020-07-14 21:44:11 +0300

    Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -29,6 +29,37 @@
  * @constructor
  */
 export default class UserTable {
+
   constructor(rows) {
+    this.elem = this.rowsElement(rows);
   }
+  rowsElement(rows) {
+    let table = document.createElement('table')
+    table.insertAdjacentHTML('afterbegin', `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>`)
+    rows.forEach(element => {
+      table.insertAdjacentHTML('beforeend', `
+      <tr>
+        <td> ${element.name}</td>
+        <td>${element.age}</td> 
+        <td>${element.salary}</td>
+        <td>${element.city}</td>
+        <td><button>X</button></td>
+        </tr>`)
+    });
+    document.addEventListener('click', function (e) {
+      if (e.target.tagName == table.querySelector('button').tagName) {
+        e.target.closest('tbody').remove()
+      }
+    })
+    return table
+  };
 }
